2e4a51a42cb456a1b0e288f6bb7a465c4c398b2c
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-20 14:59:43 +0530

    Initial Commit

---
+++ SideBar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SideBar
+//
+//  Created by Bhumika Patel on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideBar/SideBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideBarApp.swift
+//  SideBar
+//
+//  Created by Bhumika Patel on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SideBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
