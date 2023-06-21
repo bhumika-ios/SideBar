db7034fda9ac37db2b86a2b6dbd960cc6c60f317
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-21 12:16:08 +0530

    add url image for fetch image

--- SideBar/Home.swift
+++ SideBar/Home.swift
@@ -31,10 +31,33 @@ struct Home_Previews: PreviewProvider {
 struct HomePage: View{
     var body: some View{
         NavigationView{
-            Text("Home")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .navigationTitle("Home")
+            GeometryReader { dimensions in
+                List {
+                    ForEach(0..<8) { _ in
+                        AsyncImage(url: URL(string: "https://picsum.photos/600")) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 240)
+                        } placeholder: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(.gray.opacity(0.6))
+                                    .frame(height: 240)
+                                ProgressView()
+                            }
+                        }
+                        .aspectRatio(3 / 2, contentMode: .fill)
+                        .cornerRadius(12)
+                        .padding(.vertical)
+                        .shadow(radius: 4)
+                    }
+                    .listStyle(.inset)
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                }
+                
+            }
         }
     }
 }
