21992af14401c63d6e175e64eccd89fbb7641c58
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-07-06 15:25:36 +0530

    add navigation view

--- SideBar/MainView.swift
+++ SideBar/MainView.swift
@@ -12,53 +12,59 @@ struct MainView: View {
     @State var show = false
     
     var body: some View {
-        ZStack{
-            Color("Purple")
-                .edgesIgnoringSafeArea(.all)
-            
-            SideMenu(selectedTab: $selectedTab)
+        NavigationView{
             ZStack{
-                Color.white
-                    .opacity(0.5)
-                    .cornerRadius(show ? 15 : 0)
-                    .shadow(color: Color.black.opacity(0.07), radius: 5, x: -5, y: 0)
-                    .offset(x: show ? -25 : 0)
-                    .padding(.vertical,20)
-                Home(selectedTab: $selectedTab)
-                    .cornerRadius(show ? 15 : 0)
+                Color("Purple")
+                    .edgesIgnoringSafeArea(.all)
+                
+                SideMenu(selectedTab: $selectedTab)
+                ZStack{
+                    Color.white
+                        .opacity(0.5)
+                        .cornerRadius(show ? 15 : 0)
+                        .shadow(color: Color.black.opacity(0.07), radius: 5, x: -5, y: 0)
+                        .offset(x: show ? -25 : 0)
+                        .padding(.vertical,20)
+                    Home(selectedTab: $selectedTab)
+                        .cornerRadius(show ? 15 : 0)
                     
-            }
-            .scaleEffect(show ? 0.84 : 1)
-            .offset(x: show ? getRect().width - 220 : 0)
-            .ignoresSafeArea()
-            .overlay(
-                Button(action: {
-                    withAnimation(.spring()){
-                        show.toggle()
-                    }
-                }, label: {
-                    VStack(spacing: 5){
-                        Capsule()
-                            .fill(show ? Color.white : Color.primary)
-                            .frame(width: 30, height: 3)
-                            .rotationEffect(.init(degrees: show ? -50 : 0))
-                            .offset(x: show ? 2 : 0, y: show ? 9 : 0)
+                    
+                }
+                .scaleEffect(show ? 0.84 : 1)
+                .offset(x: show ? getRect().width - 220 : 0)
+                
+                .ignoresSafeArea()
+                
+                
+                .overlay(
+                    Button(action: {
+                        withAnimation(.spring()){
+                            show.toggle()
+                        }
+                    }, label: {
                         VStack(spacing: 5){
                             Capsule()
                                 .fill(show ? Color.white : Color.primary)
                                 .frame(width: 30, height: 3)
-                            Capsule()
-                                .fill(show ? Color.white : Color.primary)
-                                .frame(width: 30, height: 3)
-                                .offset(y: show ? -8 : 0)
+                                .rotationEffect(.init(degrees: show ? -50 : 0))
+                                .offset(x: show ? 2 : 0, y: show ? 9 : 0)
+                            VStack(spacing: 5){
+                                Capsule()
+                                    .fill(show ? Color.white : Color.primary)
+                                    .frame(width: 30, height: 3)
+                                Capsule()
+                                    .fill(show ? Color.white : Color.primary)
+                                    .frame(width: 30, height: 3)
+                                    .offset(y: show ? -8 : 0)
+                            }
+                            .rotationEffect(.init(degrees: show ? 50 : 0))
                         }
-                        .rotationEffect(.init(degrees: show ? 50 : 0))
-                    }
-                })
-                .offset(y: -8)
-                .padding()
-                ,alignment: .topLeading
-            )
+                    })
+                    .offset(y: -8)
+                    .padding()
+                    ,alignment: .topLeading
+                )
+            }
         }
     }
 }
