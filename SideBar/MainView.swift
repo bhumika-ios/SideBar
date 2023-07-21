46b5ac2cdeadae7978eb8b00e8441e855995b296
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-07-21 17:41:30 +0530

    change some code

--- SideBar/MainView.swift
+++ SideBar/MainView.swift
@@ -12,29 +12,38 @@ struct MainView: View {
     @State var show = false
     
     var body: some View {
-        NavigationView{
-            ZStack{
+        NavigationView {
+            ZStack {
                 Color("Purple")
                     .edgesIgnoringSafeArea(.all)
                 
                 SideMenu(selectedTab: $selectedTab)
-                ZStack{
+                
+                ZStack {
                     Color.white
                         .opacity(0.5)
                         .cornerRadius(show ? 15 : 0)
                         .shadow(color: Color.black.opacity(0.07), radius: 5, x: -5, y: 0)
                         .offset(x: show ? -25 : 0)
-                        .padding(.vertical,20)
-                    Home(selectedTab: $selectedTab)
-                        .cornerRadius(show ? 15 : 0)
+                        .padding(.vertical, 20)
                     
-                    
+                    // Wrap the Home view in a VStack to allow tapping outside the content
+                    VStack {
+                        if selectedTab == "Home" {
+                            HomePage()
+                        } else if selectedTab == "Setting" {
+                            Setting()
+                        } else if selectedTab == "Notification" {
+                            Notification()
+                        } else if selectedTab == "Help" {
+                            Help()
+                        }
+                    }
+                    .cornerRadius(show ? 15 : 0)
                 }
                 .scaleEffect(show ? 0.84 : 1)
                 .offset(x: show ? getRect().width - 220 : 0)
-                
                 .ignoresSafeArea()
-                
                 
                 .overlay(
                     Button(action: {
@@ -42,13 +51,13 @@ struct MainView: View {
                             show.toggle()
                         }
                     }, label: {
-                        VStack(spacing: 5){
+                        VStack(spacing: 5) {
                             Capsule()
                                 .fill(show ? Color.white : Color.primary)
                                 .frame(width: 30, height: 3)
                                 .rotationEffect(.init(degrees: show ? -50 : 0))
                                 .offset(x: show ? 2 : 0, y: show ? 9 : 0)
-                            VStack(spacing: 5){
+                            VStack(spacing: 5) {
                                 Capsule()
                                     .fill(show ? Color.white : Color.primary)
                                     .frame(width: 30, height: 3)
@@ -61,13 +70,15 @@ struct MainView: View {
                         }
                     })
                     .offset(y: -8)
-                    .padding()
-                    ,alignment: .topLeading
+                    .padding(),
+                    alignment: .topLeading
                 )
             }
         }
     }
 }
+
+// The rest of the code remains the same
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
